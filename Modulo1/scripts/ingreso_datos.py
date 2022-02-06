145f6feacb52a25b50ef39f5849e99abef7b148a
gdelgador <gonzalo.delgado.r@uni.pe>
2022-02-06 13:19:19 -0500

    update modulo1

---
+++ Modulo1/scripts/ingreso_datos.py
@@ -0,0 +1,7 @@
+
+# control + s -> guardar cambios
+numero = int(input("Ingrese un numero entero: "))
+
+a = numero + 2
+
+print(a)
---
+++ Modulo1/scripts/practica1.py
@@ -0,0 +1,11 @@
+
+
+
+# 1. 
+
+solicion1
+
+
+# 2.add()
+
+solucuon 2
