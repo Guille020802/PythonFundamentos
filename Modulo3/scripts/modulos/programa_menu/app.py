f045f748fb5da46797f39d2f2e7209027afa0908
gdelgador <gonzalo.delgado.r@uni.pe>
2022-04-24 12:10:47 -0500

    update menu

--- Modulo3/scripts/modulos/programa_menu/app.py
+++ Modulo3/scripts/modulos/programa_menu/app.py
@@ -1,34 +1,36 @@
 # 1. Librerias
 import validacion_datos as v # 'v' es un alias para referirme al modulo 'validacion_datos'
-
 # from "nombre_sub_carpeta" import "nombre_archivo" as 'alias'
 from funciones_avanzadas import maths_functions as m 
+# import 'nombre_sub_carpeta.nombre_script' as 'alias'
+import funciones_avanzadas.triangulos as t
+# from 'carpeta' import 'scrip1', 'script2'
+from funciones_avanzadas import maths_functions, triangulos
+
+# from 'carpeta'.'script' import 'funcion'
+from basics.operaciones_basicas import sumar
 
 # 2. Constantes
-
-
-# 3. Funciones y/o clases
-
-def sumar(x: int, y: int)->int :
-    """ Retorna la suma de x + y"""
-    return x + y
-
-def main():
-        
-    print("Bienvenido al menú interactivo")
-
-    while True:
-        print("""¿Qué quieres hacer? Escribe una opción
+MSG_BIENVENIDA = "Bienvenido al menú interactivo"
+MSG_OPCIONES = """¿Qué quieres hacer? Escribe una opción
         1) Sumar 2 numeros enteros
         2) Calcular el factorial de un numero
         3) Área de la circunferencia
         4) Mostrar un triangulo
         5) Evaluar si un numero es primo
-        6) Salir""")
+        6) Salir
+        """
+
+# 3. Funciones y/o clases
+
+def main():
+    
+    print(MSG_BIENVENIDA)
+
+    while True:
         
-        opcion = input() # me devuelve un string ''
+        opcion = input(MSG_OPCIONES) # me devuelve un string ''
         if opcion == '1':
-            
             numero1 = v.ingreso_entero("Ingrese el primero número entero: ")
             numero2 = v.ingreso_entero("Ingrese el segundo número entero: ")
             
@@ -44,9 +46,14 @@ def main():
             print(f'El factorial de {n} es {factorial}')
             
         elif opcion == '3':
-            pass
+            radio = v.ingreso_decimal('Ingrese el radio del circulo: ')
+            area = m.area_circulo(radio)
+            print(f'El área de la circunferencia es: {area}')
+            
         elif opcion == '4':
-            pass
+            altura = v.ingreso_entero('Ingrese la altura del triangulo: ')
+            triangulos.mostrar_triangulo(altura)
+            
         elif opcion == '5':
             pass
         elif opcion =='6':
@@ -57,7 +64,8 @@ def main():
     pass
 
 # 4. Principal
-try:
-    main()
-finally:
-    print('Programa Finalizado')
+if __name__ == '__main__':
+    try:
+        main()
+    finally:
+        print('Programa Finalizado')
--- Modulo3/scripts/modulos/programa_menu/funciones_avanzadas/maths_functions.py
+++ Modulo3/scripts/modulos/programa_menu/funciones_avanzadas/maths_functions.py
@@ -1,4 +1,4 @@
-
+import math
 
 def calcular_factorial(n: int)->int:
     """Calcula el factorial de un número n"""
@@ -14,10 +14,28 @@ def calcular_factorial_recursivo(n: int)->int:
     elif n > 1:
         return n * calcular_factorial_recursivo(n-1)
     
+def area_circulo(radio: float, pi: float=3.14)->float:
+    """Calcula el área de un circulo
+
+    Args:
+        radio (float): Radio del circulo
+        pi (float, optional): Valor de PI. Defaults to 3.14.
+        
+    Returns:
+        float: Area del circulo
+    """
+    potencia = math.pow(radio,2)
+    return pi * potencia
 
 # pruebo mis funciones
-x = calcular_factorial(5)
-print(x == 120) # 
+if __name__ == '__main__':
+    x = calcular_factorial(5)
+    print(x == 120) # 
 
-y = calcular_factorial_recursivo(3)
-print(y == 6) # 
+    y = calcular_factorial_recursivo(3)
+    print(y == 6) # 
+
+
+    # prueba
+    a = area_circulo(1)
+    print(a)
