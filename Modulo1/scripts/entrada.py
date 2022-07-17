2fc1725bfad6d6d86e6384f4f0bba5859e3e89b6
gdelgador <gonzalo.delgado.r@uni.pe>
2022-07-17 13:27:09 -0500

    modulo1

---
+++ Modulo1/scripts/entrada.py
@@ -0,0 +1,14 @@
+
+
+x = int(input('Ingrese un numero entero: '))
+
+suma = x + 7
+
+print(f'La suma de {x} + 7 = {suma}')
+print('La suma de {} + 7 = {}'.format(x, suma))
+print('La suma de {x} + 7 = {s}'.format(x=x, s=suma))
+
+
+
+
+
---
+++ Modulo1/scripts/hola.py
@@ -0,0 +1,7 @@
+# control + n -> genera nuevo archivo
+# control + s -> guardar cambios del archivo 
+
+
+print('hola mundo')
+
+x = 5
